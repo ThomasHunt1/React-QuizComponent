13aa18a7aefdce23ee0565aefbf0e011e7a10cbb
xrb16171 <xrb16171@uni.strath.ac.uk>
2020-06-29 14:23:02 +0100

    Module 5 complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,9 +16,14 @@ class Quiz extends Component {
             <div>
                 {isQuizEnd ? <QuizEnd /> : <QuizQuestion 
                 quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                showNextQuestionHandler ={this.showNextQuestion.bind(this)}
                 />}
             </div>
         );
+    }
+
+    showNextQuestion(){
+        this.setState({quiz_position: this.state.quiz_position + 1});
     }
 }
 
